023bec23803bef0f5e7c5009cae8ec003e472ef6
Leangeful <cuttheleash1@gmail.com>
2023-11-28 18:29:55 +0100

    init window and openGL context

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,18 @@
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <string>
 
 int windowWidth = 800;
 int windowHeight = 600;
+const std::string windowTitle = "OpenGL Window";
 
 SDL_Window* window = nullptr;
 SDL_GLContext glContext = nullptr;
 
-void initializeProgram() {
-  int err = SDL_Init(SDL_INIT_VIDEO);
-  if (err < 0) {
-    std::cout << "ERROR::SDL::INIT::VIDEO " << err << std::endl;
-    cleanUp();
-    exit(err);
-  }
-};
-
-void mainLoop(){};
-
-void cleanUp() { SDL_Quit(); };
+void initializeProgram();
+void mainLoop();
+void cleanUp();
 
 int main() {
   initializeProgram();
@@ -31,3 +24,39 @@ int main() {
   std::cout << "Hello" << std::endl;
   return 0;
 }
+
+void initializeProgram() {
+  int err = SDL_Init(SDL_INIT_VIDEO);
+  if (err < 0) {
+    std::cout << "ERROR::SDL::INIT::VIDEO " << err << std::endl;
+    exit(err);
+  }
+
+  window = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight,
+                            SDL_WINDOW_OPENGL);
+
+  if (window == nullptr) {
+    std::cout << "ERROR::SDL::INIT::WINDOW" << std::endl;
+    cleanUp();
+    exit(-1);
+  }
+
+  glContext = SDL_GL_CreateContext(window);
+
+  if (!glContext) {
+    const char* err = SDL_GetError();
+    std::cout << "ERROR::SDL::INIT::CONTEXT" << std::endl << err << std::endl;
+    cleanUp();
+    exit(-1);
+  }
+};
+
+void mainLoop() {
+  while (true) {
+    char c;
+    std::cin >> c;
+  }
+};
+
+void cleanUp() { SDL_Quit(); };
