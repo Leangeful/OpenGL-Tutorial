d037db78ce76bd5bb720c77ff1e4861f4de62cdc
Leangeful <cuttheleash1@gmail.com>
2023-11-28 19:03:09 +0100

    setting gl attributes

--- src/main.cpp
+++ src/main.cpp
@@ -32,9 +32,16 @@ void initializeProgram() {
     exit(err);
   }
 
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
   window = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_CENTERED,
                             SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight,
-                            SDL_WINDOW_OPENGL);
+                            SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 
   if (window == nullptr) {
     std::cout << "ERROR::SDL::INIT::WINDOW" << std::endl;
